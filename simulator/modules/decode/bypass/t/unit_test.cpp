2803ad1f81a2835be672fed9b1df96e18afd722a
Egor Bova <bova.ev@phystech.edu>
2019-04-20 17:54:07 +0300

    Add unit test file for the bypass

---
+++ simulator/modules/decode/bypass/t/unit_test.cpp
@@ -0,0 +1,10 @@
+/**
+ * Unit test for Bypass module
+ * @author Egor Bova
+ * Copyright 2019 MIPT-MIPS team
+ */
+
+#include "../data_bypass.h"
+#include "../data_bypass_interface.h"
+#include <catch.hpp>
+

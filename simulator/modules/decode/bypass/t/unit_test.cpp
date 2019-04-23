83357cc3ef18823f5c93610cc8681a15a3d4980e
Egor Bova <bova.ev@phystech.edu>
2019-04-23 12:18:38 +0300

    Add first test case for bypass module(MIPT-ILab#716)

--- simulator/modules/decode/bypass/t/unit_test.cpp
+++ simulator/modules/decode/bypass/t/unit_test.cpp
@@ -6,5 +6,21 @@
 
 #include "../data_bypass.h"
 #include "../data_bypass_interface.h"
+
+#include <mips/mips.h>
+#include <mips/mips_instr.h>
 #include <catch.hpp>
 
+class MIPS32Instr : public BaseMIPSInstr<uint32>
+{
+public:
+	explicit MIPS32Instr( std::string_view str_opcode) : BaseMIPSInstr<uint32>( MIPSVersion::v32, str_opcode, Endian::little, 0x0000, 0xc000) { }
+};
+
+TEST_CASE( "Test test_case")
+{
+	DataBypass<MIPS32Instr> db( 7);
+	MIPS32Instr load( "lw");
+	db.trace_new_instr( load);
+	CHECK( 1);
+}
